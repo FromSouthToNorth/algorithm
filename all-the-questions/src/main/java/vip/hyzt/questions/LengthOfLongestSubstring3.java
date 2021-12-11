c45fd0d07f203bf5c0dc936a28c8c0cdf2f31565
huangyu <598050554@qq.com>
2021-12-11 10:07:17 +0800

    添加 最大子字符串 注释

--- all-the-questions/src/main/java/vip/hyzt/questions/LengthOfLongestSubstring3.java
+++ all-the-questions/src/main/java/vip/hyzt/questions/LengthOfLongestSubstring3.java
@@ -27,26 +27,28 @@ public abstract class LengthOfLongestSubstring3 {
      */
     public static int slidingWindow(String str) {
 
+        // 返回的结果
         int result = 0;
-        int start = 0;
+        // 左指针(迭代 s 字符串的指针)
         int end = 0;
+        // 滑动窗口的开始指针
+        int start = 0;
+        // 更新最长子字符串的长度
         int length = 0;
+        // 字符串长度
         int size = str.length();
-        for (;end < size; end++) {
-            // 迭代 str 字符串当中的子字符
+        for (; end < size; end++) {
+            // 迭代字符串当中的每个子字符
             char c = str.charAt(end);
-            // 模拟滑动窗口
+            // 模拟滑动窗口, 起始位置(指针)为 start 终止位置(指针)为 end
             for (int i = start; i < end; i++) {
-                // 当字符重复出现
+                // 当有子字符重复出现 更新起始位置及最长子字符串的长度
                 if (c == str.charAt(i)) {
-                    // 更新滑动窗口的起始下标
                     start = i + 1;
-                    // 更新不重复子字符串的长度
                     length = end - start;
                     break;
                 }
             }
-            // 当没有重复子字符，增加子字符串长度
             length++;
             result = Math.max(result, length);
         }
@@ -61,16 +63,29 @@ public abstract class LengthOfLongestSubstring3 {
      */
     public static int hashSlidingWindow(String str) {
 
+        // 返回的结果
         int result = 0;
-        int start = 0;
+        // 左指针 迭代 s 字符串子字符的指针
         int end = 0;
+        // 右指针 滑动窗口的起始指针
+        int start = 0;
+        // 子字符串的长度
         int length = 0;
+        // 字符串 s 的大小
         int size = str.length();
+        // 【用来记录重复子字符的 HashMap 来进行优化一层迭代，当然还有利用 ASCII 码桶 来替代 HashMap】
         Map<Character, Integer> hashMap = new HashMap<>();
-        for (;end < size; end++) {
+
+        // 迭代 s 字符串
+        for (; end < size; end++) {
+            // 取出 s 字符串当中的子字符
             char c = str.charAt(end);
+            // 模拟滑动窗口 当 hashMap 当中包含重复子字符
+            // 并且该重复子字符的下标大于滑动窗口起始下标。
             if (hashMap.containsKey(c) && hashMap.get(c) >= start) {
+                // 更新 滑动窗口的起始下标
                 start = hashMap.get(c) + 1;
+                // 更新 无重复子字符串长度
                 length = end - start;
             }
             hashMap.put(c, end);
@@ -96,6 +111,7 @@ public abstract class LengthOfLongestSubstring3 {
         Arrays.fill(asciiArray, -1);
         while (end < size) {
             char c = str.charAt(end);
+            // 当子字符下标大于滑动窗口起始下标
             if (asciiArray[c] >= start) {
                 start = asciiArray[c] + 1;
                 length = end - start;
