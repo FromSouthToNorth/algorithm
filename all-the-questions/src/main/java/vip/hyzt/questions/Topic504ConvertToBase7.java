9b40dbaedcea781fc7ccd51eea4011dee68806a2
huangyu <598050554@qq.com>
2022-03-07 10:26:56 +0800

    添加504.七进制数

---
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic504ConvertToBase7.java
@@ -0,0 +1,44 @@
+package vip.hyzt.questions;
+
+/**
+ * <h3>504. 七进制数</h3>
+ * <p>给定一个整数 num，将其转化为 7 进制，并以字符串形式输出。</p>
+ * <b>示例 1:</b>
+ * <pre>
+ *     <strong>输入: </strong>num = 100
+ *     <strong>输出: </strong>"202"
+ * </pre>
+ * <b>示例 2:</b>
+ * <pre>
+ *     <strong>输入: </strong>num = -7
+ *     <strong>输出: </strong>"-10"
+ * </pre>
+ * @see <a href="https://leetcode-cn.com/problems/base-7">504. 七进制数</a>
+ * @author 力扣（LeetCode）
+ * @author hy
+ */
+public class Topic504ConvertToBase7 {
+
+    public static String convertToBase2(int num) {
+        boolean flag = num < 0;
+        StringBuilder sb = new StringBuilder();
+        do {
+            sb.append(num % 2);
+            num /= 2;
+        } while (num != 0);
+        sb.reverse();
+        return flag ? "-" + sb : sb.toString();
+    }
+
+    public static String convertToBase7(int num) {
+        boolean flag = num < 0;
+        StringBuilder sb = new StringBuilder();
+        do {
+            sb.append(num % 7);
+            num /= 7;
+        } while (num != 0);
+        sb.reverse();
+        return flag ? "-" + sb : sb.toString();
+    }
+
+}
---
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic504ConvertToBase7Tests.java
@@ -0,0 +1,19 @@
+package vip.hyzt.questions;
+
+import org.junit.Test;
+
+public class Topic504ConvertToBase7Tests {
+
+    @Test
+    public void convertToBase7() {
+        String s = Topic504ConvertToBase7.convertToBase7(7);
+        System.out.println(s);
+    }
+
+    @Test
+    public void convertToBase2() {
+        String s = Topic504ConvertToBase7.convertToBase2(10);
+        System.out.println(s);
+    }
+
+}
