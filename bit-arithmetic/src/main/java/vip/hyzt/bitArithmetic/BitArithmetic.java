b2fa92bb0e8f22ba79dd287ce499a3563899c3fd
huangyu <598050554@qq.com>
2022-04-11 16:13:50 +0800

    添加位运算

--- bit-arithmetic/src/main/java/vip/hyzt/bitArithmetic/BitArithmetic.java
+++ bit-arithmetic/src/main/java/vip/hyzt/bitArithmetic/BitArithmetic.java
@@ -41,7 +41,7 @@ public abstract class BitArithmetic {
      * <p><b>示例 1：</b></p>
      * <pre>
      *     <strong>输入：</strong>num = 5, bit = 1
-     *     <strong>输出：</strong>20
+     *     <strong>输出：</strong>2
      *     <strong>解释：</strong>
      *     `5` 转换二进制为：0000 0000 0000 0000 0000 0000 0000 0101
      *     向左移一位为:     <i style="color:red">0</i>000 0000 0000 0000 0000 0000 0000 0010
@@ -51,7 +51,7 @@ public abstract class BitArithmetic {
      * <p><b>示例 2：</b></p>
      * <pre>
      *     <strong>输入：</strong>num = -5, bit = 2
-     *     <strong>输出：</strong>20
+     *     <strong>输出：</strong>-2
      *     <strong>解释：</strong>
      *     `-5` 转换二进制为：1111 1111 1111 1111 1111 1111 1111 1011
      *     向左移两位为:     <i style="color:red">11</i>11 1111 1111 1111 1111 1111 1111 1110
@@ -70,7 +70,7 @@ public abstract class BitArithmetic {
      * <h3>无符号右移 >>></h3>
      * <pre>
      *     <strong>输入：</strong>num = 5, bit = 1
-     *     <strong>输出：</strong>20
+     *     <strong>输出：</strong>2
      *     <strong>解释：</strong>
      *     `5` 转换二进制为：0000 0000 0000 0000 0000 0000 0000 0101
      *     向左移一位为:     <i style="color:red">0</i>000 0000 0000 0000 0000 0000 0000 0010
@@ -79,8 +79,8 @@ public abstract class BitArithmetic {
      * <p><b>示例 2：</b></p>
      * <pre>
      *     <strong>输入：</strong>num = -5, bit = 2
-     *     <strong>输出：</strong>20
-     *     <strong>解释：</strong>1073741822
+     *     <strong>输出：</strong>1073741822
+     *     <strong>解释：</strong>
      *     `-5` 转换二进制为：1111 1111 1111 1111 1111 1111 1111 1011
      *     向左移两位为:     <i style="color:red">00</i>11 1111 1111 1111 1111 1111 1111 1110
      * </pre>
@@ -90,6 +90,150 @@ public abstract class BitArithmetic {
      */
     public static int unsignedShiftRight(int num, int bit) {
         return num >>> bit;
+    }
+
+    /**
+     * <h3>位与 &</h3>
+     * <p><b>示例 1：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 5，num2 = 3
+     *     <strong>输出：</strong>1
+     *     <strong>解释：</strong>
+     *     十进制 `5` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`101`
+     *     十进制 `3` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`011`
+     *     return `1` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0001
+     * </pre>
+     * <p><b>示例 2：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 4，num2 = 3
+     *     <strong>输出：</strong>0
+     *     <strong>解释：</strong>
+     *     十进制 `4` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`100`
+     *     十进制 `3` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`011`
+     *     return `0` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0000
+     * </pre>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 6，num2 = 4
+     *     <strong>输出：</strong>4
+     *     <strong>解释：</strong>
+     *     十进制 `6` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`110`
+     *     十进制 `4` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`100`
+     *     return `4` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0100
+     * </pre>
+     * @param num1 第一个操作数
+     * @param num2 第二个操作数
+     * @return 如果两个操作数 n 位都为 1，结果 n 位则为 1，否则为 0
+     */
+    public static int bitAnd(int num1, int num2) {
+        return num1 & num2;
+    }
+
+    /**
+     * <h3>位或 |</h3>
+     * <p><b>示例 1：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 5, num2 = 3
+     *     <strong>输出：</strong>7
+     *     <strong>解释：</strong>
+     *     十进制 `5` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`101`
+     *     十进制 `3` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`011`
+     *     return 十进制 `7` -> 二进制:0000 0000 0000 0000 0000 0000 0000 0`0111`
+     * </pre>
+     * <p><b>示例 2：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 9, num2 = 8
+     *     <strong>输出：</strong>11
+     *     <strong>解释：</strong>
+     *     十进制 `9` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `1001`
+     *     十进制 `8` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `1000`
+     *     return 十进制 `9` -> 二进制:0000 0000 0000 0000 0000 0000 0000 `1001`
+     * </pre>
+     * <p><b>示例 3：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 4, num2 = 8
+     *     <strong>输出：</strong>12
+     *     <strong>解释：</strong>
+     *     十进制 `4` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `0100`
+     *     十进制 `8` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `1000`
+     *     return 十进制 `12` -> 二进制:0000 0000 0000 0000 0000 0000 0000 `1100`
+     * </pre>
+     * @param num1 第一个操作数
+     * @param num2 第二个操作数
+     * @return 如果两个操作数 n 位有一位为 1，那么结果的 n 位也为 1，否则为 0
+     */
+    public static int bitOr(int num1, int num2) {
+        return num1 | num2;
+    }
+
+    /**
+     * <h3>异位或 ^</h3>
+     * <p><b>示例 1：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 5, num2 = 3
+     *     <strong>输出：</strong>6
+     *     <strong>解释：</strong>
+     *     十进制 `5` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`101`
+     *     十进制 `3` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0`011`
+     *     return 十进制 `6` -> 二进制:0000 0000 0000 0000 0000 0000 0000 0`110`
+     * </pre>
+     * <p><b>示例 2：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 9, num2 = 4
+     *     <strong>输出：</strong>13
+     *     <strong>解释：</strong>
+     *     十进制 `9` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `1001`
+     *     十进制 `4` -> 二进制：0000 0000 0000 0000 0000 0000 0000 `0100`
+     *     return 十进制 `13` -> 二进制:0000 0000 0000 0000 0000 0000 0000 `1101`
+     * </pre>
+     * <p><b>示例 3：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num1 = 1, num2 = 1
+     *     <strong>输出：</strong>0
+     *     <strong>解释：</strong>
+     *     十进制 `1` -> 二进制：0000 0000 0000 0000 0000 0000 0000 000`1`
+     *     十进制 `1` -> 二进制：0000 0000 0000 0000 0000 0000 0000 000`1`
+     *     return 十进制 `0` -> 二进制:0000 0000 0000 0000 0000 0000 0000 000`0`
+     * </pre>
+     * @param num1 第一个操作数
+     * @param num2 第二个操作数
+     * @return 第一个操作数的第 `n` 位与第二个操作数的第 `n` 位相反，那么结果的 `n` 位为 `1`。相等则为 `0`
+     */
+    public static int bitXOR(int num1, int num2) {
+        return num1 ^ num2;
+    }
+
+
+    /**
+     * <h3>位非 ~</h3>
+     * <p><b>示例 1：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num = 5
+     *     <strong>输出：</strong>-6
+     *     <strong>解释：</strong>
+     *     十进制 `5` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0101
+     *     return 十进制 `-6` -> 二进制: 1111 1111 1111 1111 1111 1111 1111 1010
+     * </pre>
+     * <p><b>示例 2：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num = 9
+     *     <strong>输出：</strong>-10
+     *     <strong>解释：</strong>
+     *     十进制 `9` -> 二进制：0000 0000 0000 0000 0000 0000 0000 1001
+     *     return 十进制 `-10` -> 二进制：1111 1111 1111 1111 1111 1111 1111 0110
+     * </pre>
+     * <p><b>示例 3：</b></p>
+     * <pre>
+     *     <strong>输入：</strong>num = 1
+     *     <strong>输出：</strong>-2
+     *     <strong>解释：</strong>
+     *     十进制 `1` -> 二进制：0000 0000 0000 0000 0000 0000 0000 0001
+     *     return 十进制 `-2` -> 二进制: 1111 1111 1111 1111 1111 1111 1111 1110
+     * </pre>
+     * @param num 需要操作的数
+     * @return 操作数第 `n` 位为 `1`，那么结果的第 `n` 位为 `0`。反之 结果第 `n` 位为 `1`
+     */
+    public static int bitNot(int num) {
+        return ~num;
     }
 
 }
--- bit-arithmetic/src/test/java/vip/hyzt/bitArithmetic/BitArithmeticTests.java
+++ bit-arithmetic/src/test/java/vip/hyzt/bitArithmetic/BitArithmeticTests.java
@@ -6,20 +6,47 @@ public class BitArithmeticTests {
 
     @Test
     public void shiftLeft() {
-        int num = BitArithmetic.shiftLeft(-5, 2);
-        System.out.println(num);
+        System.out.println("5 << 1 = " + BitArithmetic.shiftLeft(5, 1));
+        System.out.println("-5 << 2 = " + BitArithmetic.shiftLeft(-5, 2));
     }
 
     @Test
     public void shiftRight() {
-        int num = BitArithmetic.shiftRight(-5, 2);
-        System.out.println(num);
+        System.out.println("5 >> 1 = " + BitArithmetic.shiftRight(5, 1));
+        System.out.println("-5 >> 2 = " + BitArithmetic.shiftRight(-5, 2));
     }
 
     @Test
     public void unsignedShiftRight() {
-        int num = BitArithmetic.unsignedShiftRight(-5, 2);
-        System.out.println(num);
+        System.out.println("-5 >>> 2 = " + BitArithmetic.unsignedShiftRight(-5, 2));
+    }
+
+    @Test
+    public void bitAnd() {
+        System.out.println("5 & 3 = " + BitArithmetic.bitAnd(5, 3));
+        System.out.println("4 & 3 = " + BitArithmetic.bitAnd(4, 3));
+        System.out.println("6 & 4 = " + BitArithmetic.bitAnd(6, 4));
+    }
+
+    @Test
+    public void bitOr() {
+        System.out.println("5 | 3 = " + BitArithmetic.bitOr(5, 3));
+        System.out.println("9 | 8 = " + BitArithmetic.bitOr(9, 8));
+        System.out.println("4 | 8 = " + BitArithmetic.bitOr(4, 8));
+    }
+
+    @Test
+    public void bitXOR() {
+        System.out.println("5 ^ 3 = " + BitArithmetic.bitXOR(5, 3));
+        System.out.println("9 ^ 4 = " + BitArithmetic.bitXOR(9, 4));
+        System.out.println("1 ^ 1 = " + BitArithmetic.bitXOR(1, 1));
+    }
+
+    @Test
+    public void bitNot() {
+        System.out.println("~5 = " + BitArithmetic.bitNot(5));
+        System.out.println("~9 = " + BitArithmetic.bitNot(9));
+        System.out.println("~1 = " + BitArithmetic.bitNot(1));
     }
 
 }
