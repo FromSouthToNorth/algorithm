f148fbd37b5885face14d9674c963370f2efc912
FromSouthToNorth <598050554@qq.com>
2022-04-15 22:37:55 +0800

    add

---
+++ all-the-questions/src/main/java/vip/hyzt/questions/NestedInteger.java
@@ -0,0 +1,28 @@
+package vip.hyzt.questions;
+
+import java.util.List;
+
+public interface NestedInteger {
+
+    // Constructor initializes an empty nested list.
+
+    // Constructor initializes a single integer.
+
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // Set this NestedInteger to hold a single integer.
+    public void setInteger(int value);
+
+    // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+    public void add(NestedInteger ni);
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return empty list if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+
+}
---
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic385Deserialize.java
@@ -0,0 +1,37 @@
+package vip.hyzt.questions;
+
+/**
+ * <h3>给定一个字符串 s 表示一个整数嵌套列表，实现一个解析它的语法分析器并返回解析的结果 NestedInteger 。</h3>
+ * <p>列表中的每个元素只可能是整数或整数嵌套列表</p>
+ * <p><b>示例 1：</b></p>
+ * <pre>
+ *     <strong>输入：</strong>s = "324",
+ *     <strong>输出：</strong>324
+ *     <strong>解释：</strong>你应该返回一个 NestedInteger 对象，其中只包含整数值 324。
+ * </pre>
+ * <p><b>示例 2：</b></p>
+ * <pre>
+ *     <strong>输入：</strong>s = "[123,[456,[789]]]",
+ *     <strong>输出：</strong>[123,[456,[789]]]
+ *     <strong>解释：</strong>
+ *     返回一个 NestedInteger 对象包含一个有两个元素的嵌套列表：
+ *     1. 一个 integer 包含值 123
+ *     2. 一个包含两个元素的嵌套列表：
+ *         i.  一个 integer 包含值 456
+ *         ii. 一个包含一个元素的嵌套列表
+ *              a. 一个 integer 包含值 789
+ * </pre>
+ * <p><b>提示：</b></p>
+ * <li>1 <= s.length <= 5 * 104</li>
+ * <li>s 由数字、方括号"[]"、负号'-'、逗号','组成</li>
+ * <li>用例保证s 是可解析的NestedInteger</li>
+ * <li>输入中的所有值的范围是[-10<sup>6</sup>, 10<sup>6</sup>]</li>
+ * @see <a href="https://leetcode-cn.com/problems/mini-parser">链接：https://leetcode-cn.com/problems/mini-parser</a>
+ * @author 力扣（LeetCode）
+ * @author hy
+ */
+public class Topic385Deserialize {
+
+
+
+}
