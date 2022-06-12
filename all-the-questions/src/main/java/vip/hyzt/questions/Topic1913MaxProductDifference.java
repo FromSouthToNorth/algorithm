aadf230bbbd78f98bbd59359b1f69ac0b3fca483
FromSouthToNorth <598050554@qq.com>
2022-06-12 20:32:28 +0800

    add

---
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic1913MaxProductDifference.java
@@ -0,0 +1,68 @@
+package vip.hyzt.questions;
+
+import java.util.Arrays;
+
+/**
+ * <h3>1913. 两个数对之间的最大乘积差</h3>
+ * <p>两个数对 (a, b) 和 (c, d) 之间的 乘积差 定义为 (a * b) - (c * d) 。</p>
+ * <li>例如，(5, 6) 和 (2, 7) 之间的乘积差是 (5 * 6) - (2 * 7) = 16 。</li>
+ * <p>给你一个整数数组 nums ，选出四个 不同的 下标 w、x、y 和 z ，使数对 (nums[w], nums[x]) 和 (nums[y], nums[z]) 之间的 乘积差 取到 最大值 。</p>
+ * <p>返回以这种方式取得的乘积差中的 最大值 。</p>
+ * <h3>示例 1：</h3>
+ * <pre>
+ *     输入：nums = [5,6,2,7,4]
+ *     输出：34
+ *     解释：可以选出下标为 1 和 3 的元素构成第一个数对 (6, 7) 以及下标 2 和 4 构成第二个数对 (2, 4)
+ *     乘积差是 (6 * 7) - (2 * 4) = 34
+ * </pre>
+ * <h3>示例 2：</h3>
+ * <pre>
+ *     输入：nums = [4,2,5,9,7,4,8]
+ *     输出：64
+ *     解释：可以选出下标为 3 和 6 的元素构成第一个数对 (9, 8) 以及下标 1 和 5 构成第二个数对 (2, 4)
+ *     乘积差是 (9 * 8) - (2 * 4) = 64
+ * </pre>
+ * <h3>提示：</h3>
+ * <li>4 <= nums.length <= 10<sup>4</sup></li>
+ * <li>1 <= nums[i] <= 10<sup>4</sup></li>
+ * @see <a href="https://leetcode.cn/problems/maximum-product-difference-between-two-pairs/">https://leetcode.cn/problems/maximum-product-difference-between-two-pairs/</a>
+ * @author 力扣（LeetCode）
+ * @author hy
+ */
+public class Topic1913MaxProductDifference {
+
+    public static int maxProductDifference(int[] nums) {
+        int ans = 0;
+        aux = new int[nums.length];
+        sort(nums,0, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+        ans = (nums[nums.length - 1]) * (nums[nums.length - 2]) - (nums[1] * nums[0]);
+        return ans;
+    }
+
+    static int[] aux;
+
+    static void sort(int[] nums, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2 ;
+        sort(nums, lo, mid);
+        sort(nums, mid + 1, hi);
+        merge(nums, lo, mid, hi);
+    }
+
+    static void merge(int[] nums, int lo, int mid, int hi) {
+        int i = lo, j = mid + 1;
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = nums[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)           nums[k] = aux[j++];
+            else if (j > hi)            nums[k] = aux[i++];
+            else if (aux[j] < aux[i])   nums[k] = aux[j++];
+            else                        nums[k] = aux[i++];
+        }
+    }
+
+}
---
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic1913MaxProductDifferenceTests.java
@@ -0,0 +1,14 @@
+package vip.hyzt.questions;
+
+import org.junit.Test;
+
+public class Topic1913MaxProductDifferenceTests {
+
+    @Test
+    public void maxProductDifference() {
+        int [] nums = new int[] {5,6,2,7,4};
+        int ans = Topic1913MaxProductDifference.maxProductDifference(nums);
+        System.out.println(ans);
+    }
+
+}
