32e57f14c540ce1ad30643a988d25ed71b4d48b1
huangyu <598050554@qq.com>
2022-07-05 16:51:39 +0800

    add

---
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic729MyCalendar.java
@@ -0,0 +1,29 @@
+package vip.hyzt.questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <h3>729. 我的日程安排表 I</h3>
+ * @see <a href="https://leetcode.cn/problems/my-calendar-i/">https://leetcode.cn/problems/my-calendar-i/</a>
+ * @author hy
+ */
+public class Topic729MyCalendar {
+
+    private List<int[]> list;
+
+    public Topic729MyCalendar() {
+        list = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+        for (int[] ints : list) {
+            if (ints[0] > end && ints[1] > start) {
+                return false;
+            }
+        }
+        list.add(new int[]{ start, end });
+        return true;
+    }
+
+}
