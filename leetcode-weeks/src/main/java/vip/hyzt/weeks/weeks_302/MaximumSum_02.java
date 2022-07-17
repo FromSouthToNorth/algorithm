7e781550387c300b73ebbf6dde1e02ff8f378d93
FromSouthToNorth <598050554@qq.com>
2022-07-17 15:28:50 +0800

    add

---
+++ leetcode-weeks/src/main/java/vip/hyzt/weeks/weeks_302/MaximumSum_02.java
@@ -0,0 +1,28 @@
+package vip.hyzt.weeks.weeks_302;
+
+import java.util.*;
+
+/**
+ * <h3>6164. 数位和相等数对的最大和</h3>
+ * @see <a href="https://leetcode.cn/problems/max-sum-of-a-pair-with-equal-sum-of-digits/">https://leetcode.cn/problems/max-sum-of-a-pair-with-equal-sum-of-digits/</a>
+ * @author hy
+ */
+public class MaximumSum_02 {
+
+    public int maximumSum(int[] nums) {
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for (int num : nums) {
+            int sum = 0;
+            for (char c : ("" + num).toCharArray()) {
+                sum += c - '0';
+            }
+            map.computeIfAbsent(sum, t -> new PriorityQueue<>()).offer(-num);
+        }
+        int max = -1;
+        for (PriorityQueue<Integer> queue : map.values()) {
+            max = Math.max(max, queue.size() > 1 ? -queue.poll() - queue.poll() : -1);
+        }
+        return max;
+    }
+
+}
