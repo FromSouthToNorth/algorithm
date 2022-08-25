867af2317e3919572345d9185850d7510d3d0225
huangyu <598050554@qq.com>
2022-08-25 09:51:30 +0800

    添加每日一题: 658. 找到 K 个最接近的元素

--- all-the-questions/src/test/java/vip/hyzt/questions/Topic658FindClosestElementsTests.java
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic658FindClosestElementsTests.java
@@ -1,2 +1,17 @@
-package vip.hyzt.questions;public class Topic658FindClosestElementsTests {
+package vip.hyzt.questions;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class Topic658FindClosestElementsTests {
+
+    @Test
+    public void findClosestElements() {
+        Topic658FindClosestElements topic658FindClosestElements = new Topic658FindClosestElements();
+        int arr[] = {1, 2, 3, 4, 5}, k = 4, x = 3;
+        List<Integer> closestElements = topic658FindClosestElements.findClosestElements(arr, k, x);
+        System.out.println(closestElements);
+    }
+
 }
