e99b51e9873c5ae48ed0f95ec23ca92130bb5f4a
huangyu <598050554@qq.com>
2022-08-26 10:33:40 +0800

    ..

--- all-the-questions/src/main/java/vip/hyzt/questions/Topic1464MaxProduct.java
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic1464MaxProduct.java
@@ -1,7 +1,32 @@
 package vip.hyzt.questions;
 
 /**
- * <h3>1464.数组中两元素的最大乘积</h3>
+ * <h2>1464.数组中两元素的最大乘积</h2>
+ * <p>给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。</p>
+ * <p>请你计算并返回该式的最大值。</p>
+ * <h3>示例 1：</h3>
+ * <pre>
+ *     输入：nums = [3,4,5,2]
+ *     输出：12
+ *     解释：如果选择下标 i=1 和 j=2（下标从 0 开始），则可以获得最大值，(nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12 。
+ * </pre>
+ * <h3>示例 2：</h3>
+ * <pre>
+ *     输入：nums = [1,5,4,5]
+ *     输出：16
+ *     解释：选择下标 i=1 和 j=3（下标从 0 开始），则可以获得最大值 (5-1)*(5-1) = 16 。
+ * </pre>
+ * <h3>示例 3：</h3>
+ * <pre>
+ *     输入：nums = [3,7]
+ *     输出：12
+ * </pre>
+ * <h3>提示：</h3>
+ * <ul>
+ *     <li>2 <= nums.length <= 500</li>
+ *     <li>1 <= nums[i] <= 10^3</li>
+ * </ul>
+ * @see <a href="https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array">https://leetcode.cn/problems/maximum-product-of-two-elements-in-an-array</a>
  * @author 力扣（LeetCode）
  * @author hy
  */
---
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic1464MaxProductTests.java
@@ -0,0 +1,14 @@
+package vip.hyzt.questions;
+
+import org.junit.Test;
+
+public class Topic1464MaxProductTests {
+
+    @Test
+    public void maxProduct() {
+        int[] nums = {3, 4, 5, 2};
+        int i = Topic1464MaxProduct.maxProduct(nums);
+        System.out.println(i);
+    }
+
+}
