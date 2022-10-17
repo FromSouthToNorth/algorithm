7b5ba99d26d2dd1b4bc19ed31bb49fdf01319c87
huangyu <598050554@qq.com>
2022-10-17 14:05:43 +0800

    add

--- all-the-questions/src/main/java/vip/hyzt/questions/Topic6219SumOfNumberAndReverse.java
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic6219SumOfNumberAndReverse.java
@@ -52,8 +52,20 @@ public class Topic6219SumOfNumberAndReverse {
             if (i + Integer.parseInt("" + new StringBuilder("" + i).reverse()) == num) {
                 return true;
             }
+//            if (i + reverse(i) == num) {
+//                return true;
+//            }
         }
         return false;
+    }
+
+    private int reverse(int num) {
+        int res = 0;
+        while (num > 0) {
+            res = res * 10 + num % 10;
+            num /= 10;
+        }
+        return res;
     }
 
 }
