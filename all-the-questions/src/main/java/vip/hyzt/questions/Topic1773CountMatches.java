f610ec427ace0de990eb1b9eb540263ed2404f1c
FromSouthToNorth <598050554@qq.com>
2022-10-29 19:16:08 +0800

    update

--- all-the-questions/src/main/java/vip/hyzt/questions/Topic1773CountMatches.java
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic1773CountMatches.java
@@ -1,6 +1,8 @@
 package vip.hyzt.questions;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * <h3>1773. 统计匹配检索规则的物品数量</h3>
@@ -10,18 +12,13 @@ import java.util.List;
 public class Topic1773CountMatches {
 
     public static int countMatches(List<List<String>> items, String key, String value) {
-        int res = 0, typeIndex = 0;
-        switch (key) {
-            case "type":
-                break;
-            case "color":
-                typeIndex = 1;
-                break;
-            case "name":
-                typeIndex = 2;
-                break;
-            default:
-        }
+        int res = 0;
+        int typeIndex = new HashMap<String, Integer>(3) {{
+            put("type", 0);
+            put("color", 1);
+            put("name", 2);
+        }}.get(key);
+
         for (List<String> item : items) {
             if (item.get(typeIndex).equals(value)) {
                 res++;
