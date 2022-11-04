e7d934a64dcea6cde01f68821ec4438daf73653d
FromSouthToNorth <598050554@qq.com>
2022-11-04 23:13:36 +0800

    update TopicOffer10_1Fib dp[i] = (dp[i - 1] + dp[i - 2]) % MODE

--- all-the-questions/src/test/java/vip/hyzt/questions/TopicOffer10_1FibTests.java
+++ all-the-questions/src/test/java/vip/hyzt/questions/TopicOffer10_1FibTests.java
@@ -8,7 +8,7 @@ public class TopicOffer10_1FibTests {
     public void fib() {
         TopicOffer10_1Fib topicOffer10_1Fib = new TopicOffer10_1Fib();
         long startTime = System.currentTimeMillis();
-        for (int i = 0; i <= 100000; i++) {
+        for (int i = 0; i <= 100; i++) {
             System.out.printf("%d res: %d\n", i, topicOffer10_1Fib.fib(i));
         }
         long endTime = System.currentTimeMillis();
