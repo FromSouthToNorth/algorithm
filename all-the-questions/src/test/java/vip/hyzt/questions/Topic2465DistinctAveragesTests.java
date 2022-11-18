df94b8c5e62c3d8f820eede9b789750738d23b4b
huangyu <598050554@qq.com>
2022-11-18 16:54:06 +0800

    add

--- all-the-questions/src/test/java/vip/hyzt/questions/Topic2465DistinctAveragesTests.java
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic2465DistinctAveragesTests.java
@@ -10,6 +10,7 @@ public class Topic2465DistinctAveragesTests {
         System.out.printf("res: %d\n", topic2465DistinctAverages.distinctAverages(new int[]{0, 4, 3, 0, 1, 2, 8, 9, 10}));
         System.out.printf("res: %d\n", topic2465DistinctAverages.distinctAverages(new int[]{4, 1, 4, 0, 3, 5}));
         System.out.printf("res: %d\n", topic2465DistinctAverages.distinctAverages(new int[]{1, 200}));
+        System.out.printf("res: %d\n", topic2465DistinctAverages.distinctAverages(new int[]{9, 5, 7, 8, 7, 9, 8, 2, 0, 7}));
     }
 
 }
