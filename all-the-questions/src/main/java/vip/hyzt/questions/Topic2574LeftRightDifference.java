a9d6c53e2eda7ffb42a531544058d15041945d57
FromSouthToNorth <598050554@qq.com>
2023-02-26 21:55:33 +0800

    add

---
+++ all-the-questions/src/main/java/vip/hyzt/questions/Topic2574LeftRightDifference.java
@@ -0,0 +1,28 @@
+package vip.hyzt.questions;
+
+import java.util.Arrays;
+
+/**
+ * <h1>6369. 左右元素和的差值</h1>
+ *
+ * @author hy
+ * @see <a href="https://leetcode.cn/problems/left-and-right-sum-differences/description/">https://leetcode.cn/problems/left-and-right-sum-differences/description/</a>
+ */
+public class Topic2574LeftRightDifference {
+
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n], right = new int[n], res = new int[n];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] + nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] + nums[i + 1];
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = Math.abs(left[i] - right[i]);
+        }
+        return res;
+    }
+
+}
---
+++ all-the-questions/src/test/java/vip/hyzt/questions/Topic2574LeftRightDifferenceTests.java
@@ -0,0 +1,15 @@
+package vip.hyzt.questions;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class Topic2574LeftRightDifferenceTests {
+
+    @Test
+    public void leftRigthDifference() {
+        Topic2574LeftRightDifference topic2574LeftRigthDifference = new Topic2574LeftRightDifference();
+        System.out.printf("res: %s\n", Arrays.toString(topic2574LeftRigthDifference.leftRightDifference(new int[] { 10, 4, 8, 3 })));
+    }
+
+}
